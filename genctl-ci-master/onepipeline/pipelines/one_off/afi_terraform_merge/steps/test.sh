0b9efdfc93ea9bb61ef1f21e6575844235f04610
vkholic18 <106223106+vkholic18@users.noreply.github.com>
2026-09-01 11:38:28 +0530

    Modify git config to use default values for user info
    Updated git configuration commands to handle unset variables.

--- genctl-ci-master/onepipeline/pipelines/one_off/afi_terraform_merge/steps/test.sh
+++ genctl-ci-master/onepipeline/pipelines/one_off/afi_terraform_merge/steps/test.sh
@@ -64,8 +64,8 @@ echo "Selecting workspace name: $workspace_name"
 # Set the SSH - needed for core module repo clone
 eval "$(ssh-agent -s)"
 echo -e "${GIT_PRIVATE_KEY}" | ssh-add -
-git config --global user.email "${VAULT_GIT_CONFIG_USER_EMAIL}"
-git config --global user.name "${VAULT_GIT_CONFIG_USERNAME}"
+git config --global user.email "${VAULT_GIT_CONFIG_USER_EMAIL:-}"
+git config --global user.name "${VAULT_GIT_CONFIG_USERNAME:-}"
 
 # running terraform init
 echo "------------------------ Running: terraform init ------------------------"
